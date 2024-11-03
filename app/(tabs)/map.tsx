f30ef6dc206814d118572b4409a5b3aa9751b21d
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-11-03 02:09:31 +0100

    you can now update the image on a marker

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -7,7 +7,7 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage, firestore } from "@/firebaseConfig.js";
 import Button from "@/components/Button";
 import * as ImageManipulator from "expo-image-manipulator";
-import { collection, getDocs, doc, addDoc, updateDoc, onSnapshot, deleteDoc, setDoc } from "firebase/firestore";
+import { collection, doc, addDoc, updateDoc, onSnapshot, deleteDoc } from "firebase/firestore";
 
 type MarkerType = {
   id?: string | undefined;
@@ -29,23 +29,42 @@ export default function Map() {
     longitudeDelta: 20,
   });
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-  const [currentMarkerId, setCurrentMarkerId] = useState<string | null>(null); // To track the current marker
+  const [currentImage, setCurrentImage] = useState<string | undefined>(undefined);
+  const [currentMarkerId, setCurrentMarkerId] = useState<string | undefined>(undefined); // To track the current marker
 
   const mapView = useRef<MapView | null>(null); // ref. to map obj.
   const locationSubscription = useRef<Location.LocationSubscription | null>(null);
 
   useEffect(() => {
     startListening();
-    fetchMarkers();
+    const unSubFetch = fetchMarkers();
     return () => {
       if (locationSubscription.current) {
         locationSubscription.current.remove();
       }
+      if (unSubFetch) {
+        unSubFetch();
+      }
     };
   }, []);
+  // useEffect(() => {
+  //   const start = async () => {
+  //     await startListening();
+  //     const unSubFetch = await fetchMarkers();
+  //     return () => {
+  //       if (locationSubscription.current) {
+  //         locationSubscription.current.remove();
+  //       }
+  //       if (unSubFetch) {
+  //         unSubFetch();
+  //       }
+  //     };
+  //   };
+  //   start();
+  // }, []);
 
   // get collection data
-  async function fetchMarkers() {
+  function fetchMarkers() {
     try {
       // const querySnapshot = await getDocs(collection(firestore, "markers"));
       const unSub = onSnapshot(collection(firestore, "markers"), (collData) => {
@@ -61,8 +80,10 @@ export default function Map() {
           });
         });
         setMarkers(fetchedMarkers);
-        // console.log(fetchedMarkers);
+        console.log("fetchedMarkers");
+        // console.log("markers fetched: ", fetchedMarkers);
       });
+      return unSub;
     } catch (error) {
       if (error instanceof Error) {
         console.error("Error fetching markers:", error.message);
@@ -170,7 +191,6 @@ export default function Map() {
 
         // Optional: set selectedImage if needed elsewhere
         setSelectedImage(imageUri);
-        
       } else {
         Alert.alert("You did not select any image.");
       }
@@ -212,7 +232,7 @@ export default function Map() {
       console.log("Download URL obtained:", downloadURL);
 
       // Update the marker in Firestore with the new image URL
-      if (currentMarkerId !== null) {
+      if (currentMarkerId) {
         // Create a reference to the specific marker document in Firestore
         const markerDocRef = doc(firestore, "markers", currentMarkerId);
         console.log("Markerdocref: ", markerDocRef);
@@ -221,7 +241,6 @@ export default function Map() {
         await updateDoc(markerDocRef, { imageUri: downloadURL });
         console.log("Marker updated in Firestore with new image URL:", downloadURL);
         console.log("Current marker ID: ", currentMarkerId);
-        
       }
 
       Alert.alert("Image uploaded successfully!");
@@ -239,329 +258,88 @@ export default function Map() {
     }
   };
 
+  function showImage(marker: MarkerType) {
+    setCurrentImage(marker.imageUri);
+    setCurrentMarkerId(marker.id);
+  }
+
+  function updateImage() {
+    // delete old image when a new one is uploaded on a marker
+    console.log("update image on marker");
+
+    // gem reference til gammelt billede der skal slettes
+    const imageToDelete = currentImage;
+    deleteImage(imageToDelete);
+
+    // vælg nyt billede
+    pickImageAsync();
+
+    // show newly picked image and upload button
+  }
+
+  function deleteImage(imageToDelete: string | undefined) {
+    if (imageToDelete) {
+      console.log("delete image: ", imageToDelete);
+    } else {
+      console.error("no image to delete");
+    }
+  }
+
   const cancelImageSelection = () => {
     setSelectedImage(undefined); // Reset selected image
+    setCurrentImage(undefined);
   };
 
   return (
     <View style={styles.container}>
       <MapView style={styles.map} region={region} onLongPress={addMarker}>
         {markers.map((marker) => (
-          <Marker id={marker.id} coordinate={marker.coordinate} key={marker.id} title={marker.title}>
-            {marker.imageUri && (
+          <Marker
+            id={marker.id}
+            coordinate={marker.coordinate}
+            key={marker.id}
+            title={marker.title}
+            onPress={() => {
+              if (marker.imageUri) {
+                showImage(marker);
+              } else {
+                setCurrentMarkerId(marker.id);
+                pickImageAsync();
+              }
+            }}
+          >
+            {/* {marker.imageUri && (
               <Image
                 source={{ uri: marker.imageUri }}
                 style={{ width: 50, height: 50, borderRadius: 25 }} // Small version of the image
               />
-            )}
+            )} */}
           </Marker>
         ))}
       </MapView>
+
+      {currentImage && !selectedImage && (
+        <View style={styles.overlay}>
+          {/* Display existing image if no new image is selected */}
+          <Image source={{ uri: currentImage }} style={{ width: 300, height: 300 }} />
+          <Button label="Update Image" theme="select" onPress={updateImage} />
+          <Button label="Cancel" theme="cancel" onPress={cancelImageSelection} />
+        </View>
+      )}
+
       {selectedImage && (
         <View style={styles.overlay}>
+          {/* Display preview of new image for updating */}
           <Image source={{ uri: selectedImage }} style={{ width: 300, height: 300 }} />
-          <Button label="Upload Image" theme="select" onPress={uploadImage} />
+          <Button label="Upload New Image" theme="select" onPress={uploadImage} />
           <Button label="Cancel" theme="cancel" onPress={cancelImageSelection} />
         </View>
       )}
     </View>
   );
-
-  //   return (
-  //     <View style={styles.container}>
-  //       <MapView style={styles.map} region={region} onLongPress={addMarker}>
-  //         {markers.map((marker) => (
-  //           <Marker coordinate={marker.coordinate} key={marker.key} title={marker.title}>
-  //             {marker.imageUri && (
-  //               <Image
-  //                 source={{ uri: marker.imageUri }}
-  //                 style={{ width: 50, height: 50, borderRadius: 25 }} // Small version of the image
-  //               />
-  //             )}
-  //           </Marker>
-  //         ))}
-  //       </MapView>
-  //     </View>
-  //   );
 }
 
 console.log("hej");
-
-// import React, { useRef, useState, useEffect } from "react";
-// import MapView, { Marker, LongPressEvent, Region } from "react-native-maps";
-// import { StyleSheet, View, Alert } from "react-native";
-// import * as Location from "expo-location";
-// import * as ImagePicker from "expo-image-picker";
-
-// import { app, firestore, storage } from "@/firebaseConfig.js";
-// import { collection, addDoc } from "firebase/firestore";
-// import { ref, uploadBytes } from "firebase/storage";
-// import { Image } from "expo-image";
-
-// type MarkerType = {
-//   coordinate: {
-//     latitude: number;
-//     longitude: number;
-//   };
-//   key: number;
-//   title: string;
-//   imageUri?: string;
-// };
-
-// export default function Map() {
-//   const [markers, setMarkers] = useState<MarkerType[]>([]);
-//   const [region, setRegion] = useState<Region>({
-//     latitude: 55,
-//     longitude: 12,
-//     latitudeDelta: 20,
-//     longitudeDelta: 20,
-//   });
-//   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-//   const [showImagePicker, setShowImagePicker] = useState(false);
-//   const [currentMarkerKey, setCurrentMarkerKey] = useState<number | null>(null); // To track the current marker
-
-//   // useRef rerenderer IKKE når man ændrer value, men gemmer stadig value
-//   const mapView = useRef<MapView | null>(null); // ref. to map obj.
-//   const locationSubscription = useRef<Location.LocationSubscription | null>(null);
-
-//   useEffect(() => {
-//     startListening();
-//     return () => {
-//       if (locationSubscription.current) {
-//         locationSubscription.current.remove();
-//       }
-//     };
-//   }, []);
-
-//   async function startListening() {
-//     let { status } = await Location.requestForegroundPermissionsAsync();
-//     if (status !== "granted") {
-//       alert("no acess to location");
-//       return;
-//     }
-//     locationSubscription.current = await Location.watchPositionAsync(
-//       {
-//         distanceInterval: 100, // 100 meters
-//         accuracy: Location.Accuracy.High,
-//       },
-//       (location) => {
-//         const newRegion = {
-//           latitude: location.coords.latitude,
-//           longitude: location.coords.longitude,
-//           latitudeDelta: 20,
-//           longitudeDelta: 20,
-//         };
-//         setRegion(newRegion); // opdates the map with the new location
-//         if (mapView.current) {
-//           mapView.current.animateToRegion(newRegion);
-//         }
-//       }
-//     );
-//   }
-
-//   const addMarker = async (event: LongPressEvent) => {
-//     const { latitude, longitude } = event.nativeEvent.coordinate;
-//     const newMarker = {
-//       coordinate: { latitude, longitude },
-//       key: Date.now(),
-//       title: "New Marker",
-//       imageUri: undefined,
-//     };
-//     setMarkers([...markers, newMarker]);
-//     setCurrentMarkerKey(newMarker.key);
-//     await pickImageAsync();
-//   };
-
-//  const pickImageAsync = async () => {
-//    let result = await ImagePicker.launchImageLibraryAsync({
-//      allowsEditing: true,
-//      quality: 1,
-//    });
-//    /*
-//     The *******launchImageLibraryAsync()******* receives an object to specify
-//     different options. This object is the ImagePickerOptions object,
-//     which we are passing when invoking the method.
-
-//     When *******allowsEditing******* is set to true, the user can crop
-//     the image during the selection process on Android and iOS.
-
-//     */
-//    if (!result.canceled) {
-//      console.log("Image selected:", result.assets[0].uri);
-//      const imageUri = result.assets[0].uri;
-//      setSelectedImage(imageUri);
-//      await uploadImage(imageUri); // Call upload function here
-//    } else {
-//      Alert.alert("You did not select any image.");
-//    }
-//  };
-
-//   const uploadImage = async (uri: string) => {
-//     try {
-//       const response = await fetch(uri);
-//       const blob = await response.blob();
-
-//       const storageRef = ref(storage, `images/${Date.now()}.jpg`); // Create a reference to the file you want to upload
-//       await uploadBytes(storageRef, blob); // Upload the blob to Firebase Storage
-//       Alert.alert("Image uploaded successfully!");
-//     } catch (error) {
-//       if (error instanceof Error) {
-//         console.error("Error uploading image: ", error);
-//         Alert.alert("Error uploading image: ", error.message);
-//       } else {
-//         console.error("Unexpected error: ", error);
-//         Alert.alert("Unexpected error occurred.");
-//       }
-//     }
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <MapView style={styles.map} region={region} onLongPress={addMarker}>
-//         {markers.map((marker) => (
-//           <Marker coordinate={marker.coordinate} key={marker.key} title={marker.title} />
-//         ))}
-//       </MapView>
-//     </View>
-//   );
-// }
-
-// import React, { useRef, useState, useEffect } from "react";
-// import MapView, { Marker, LongPressEvent, Region } from "react-native-maps";
-// import { StyleSheet, View } from "react-native";
-// import * as Location from "expo-location";
-// import ImageViewer from "@/components/ImageViewer";
-// import Button from "@/components/Button";
-// import * as ImagePicker from "expo-image-picker";
-// import { app, firestore, storage } from "@/firebaseConfig.js";
-// import { collection, addDoc } from "firebase/firestore";
-// import { ref, uploadBytes } from "firebase/storage";
-
-// type MarkerType = {
-//   coordinate: {
-//     latitude: number;
-//     longitude: number;
-//   };
-//   key: number;
-//   title: string;
-// };
-
-// export default function map() {
-//   const [markers, setMarkers] = useState<MarkerType[]>([]);
-//   const [region, setRegion] = useState<Region>({
-//     latitude: 55,
-//     longitude: 12,
-//     latitudeDelta: 20,
-//     longitudeDelta: 20,
-//   });
-//   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-//   const pickImageAsync = async () => {
-//     let result = await ImagePicker.launchImageLibraryAsync({
-//       allowsEditing: true,
-//       quality: 1,
-//     });
-
-//     if (!result.canceled) {
-//       console.log(result);
-//       setSelectedImage(result.assets[0].uri);
-//     } else {
-//       alert("You did not select any image.");
-//     }
-//   };
-//   const PlaceholderImage = require("@/assets/images/background-image.png");
-
-//   async function uploadImage() {
-//     if (!selectedImage) {
-//       alert("No image selected");
-//       return;
-//     }
-
-//     try {
-//       const res = await fetch(selectedImage);
-//       const blob = await res.blob();
-//       const storageRef = ref(storage, "myImage.jpg");
-//       await uploadBytes(storageRef, blob);
-//       alert("Image uploaded");
-//     } catch (error) {
-//       console.error("Error uploading image:", error);
-//       alert("Failed to upload image");
-//     }
-//   }
-
-//   // useRef rerenderer IKKE når man ændrer value, men gemmer stadig value
-//   const mapView = useRef<MapView | null>(null); // ref. to map obj.
-//   const locationSubscription = useRef<Location.LocationSubscription | null>(null);
-
-//   useEffect(() => {
-//     startListening();
-//     return () => {
-//       if (locationSubscription.current) {
-//         locationSubscription.current.remove();
-//       }
-//     };
-//   }, []);
-
-//   async function startListening() {
-//     let { status } = await Location.requestForegroundPermissionsAsync();
-//     if (status !== "granted") {
-//       alert("no acess to location");
-//       return;
-//     }
-//     locationSubscription.current = await Location.watchPositionAsync(
-//       {
-//         distanceInterval: 100, // 100 meters
-//         accuracy: Location.Accuracy.High,
-//       },
-//       (location) => {
-//         const newRegion = {
-//           latitude: location.coords.latitude,
-//           longitude: location.coords.longitude,
-//           latitudeDelta: 20,
-//           longitudeDelta: 20,
-//         };
-//         setRegion(newRegion); // opdates the map with the new location
-//         if (mapView.current) {
-//           mapView.current.animateToRegion(newRegion);
-//         }
-//       }
-//     );
-//   }
-
-//   function addMarker(event: LongPressEvent) {
-//     console.log("longpress marker add");
-
-//     const { latitude, longitude } = event.nativeEvent.coordinate;
-//     const newMarker = {
-//       coordinate: { latitude, longitude },
-//       key: event.timeStamp,
-//       title: "Adding a picture...",
-//     };
-//     setMarkers([...markers, newMarker]);
-
-//   }
-
-//   function onMarkerPressed(text: string) {
-//     alert(`You pressed ` + text);
-//   }
-
-//   async function handleSave() {
-//     await addDoc(collection(firestore, "selectedImage"), {});
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <MapView style={styles.map} region={region} onLongPress={addMarker}>
-//         {markers.map((marker) => (
-//           <Marker coordinate={marker.coordinate} key={marker.key} title={marker.title} onPress={pickImageAsync} />
-//           // <Marker coordinate={marker.coordinate} key={marker.key} title={marker.title} onPress={() => onMarkerPressed(marker.title)} />
-//         ))}
-//       </MapView>
-//       <View>
-//         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-//       </View>
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
